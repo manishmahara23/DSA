c17ca8bbb40ef491a61e1e9e70310144225c736e
Manish Mahara <manish23mahara@gmail.com>
2026-01-01 22:05:25 +0530

    Add files via upload

---
+++ Arrays/MergeSortedArray.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx=m;
+        for(int i=0; i<nums2.length; i++){
+            nums1[idx++]=nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
