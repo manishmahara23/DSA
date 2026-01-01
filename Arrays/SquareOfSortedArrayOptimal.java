74a4006076a50a4f944cd08013d6e792b8817285
Manish Mahara <manish23mahara@gmail.com>
2026-01-01 22:03:32 +0530

    Add files via upload

---
+++ Arrays/SquareOfSortedArrayOptimal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n= nums.length;
+        int[] result= new int[n];
+        int left= 0;
+        int right= n-1;
+        int idx= n-1;
+        while(left<=right){
+            int leftSquare= nums[left]*nums[left];
+            int rightSquare= nums[right]* nums[right];
+        
+        if(leftSquare>rightSquare){
+            result[idx]= leftSquare;
+            left++;
+        }else{
+            result[idx]= rightSquare;
+            right--;
+        }
+        idx--;
+        }
+        return result;
+    }
+}
