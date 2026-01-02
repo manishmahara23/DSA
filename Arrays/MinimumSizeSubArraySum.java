938aee3fa3efda98005382adb6c7ae2c0579f450
Manish Mahara <manish23mahara@gmail.com>
2026-01-02 20:45:57 +0530

    Add files via upload

---
+++ Arrays/MinimumSizeSubArraySum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n= nums.length;
+        int minLen = Integer.MAX_VALUE;
+
+        for(int i=0; i<n ; i++){
+            int sum =0;
+            for(int j=i ; j<n; j++){
+                sum += nums[j];
+                
+                if(sum>= target){
+                    minLen = Math.min( minLen, j-i+1);
+                    break;
+                }
+            }
+        }
+        return minLen== Integer.MAX_VALUE ? 0: minLen;
+    }
+}
