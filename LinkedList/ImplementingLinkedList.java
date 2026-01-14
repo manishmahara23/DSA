f9b00c856196d9fe8af1f2493874ef2d2cbb5bcb
Manish Mahara <manish23mahara@gmail.com>
2026-01-14 23:27:31 +0530

    Add files via upload

---
+++ LinkedList/ImplementingLinkedList.java
@@ -0,0 +1,24 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int new_data){
+        this.data = new_data;
+        this.next = null;
+    }
+}
+public class ImplementingLinkedList {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        
+        Node temp = head;
+        while(temp !=null){
+            System.out.print(temp.data +"->");
+            temp =temp.next;
+        }
+        System.out.print("null");
+    }
+}
