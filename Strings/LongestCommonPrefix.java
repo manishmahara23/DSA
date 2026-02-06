8b5ce8f57a81931f0ea7be86b13059631f5d9798
Manish Mahara <manish23mahara@gmail.com>
2026-02-06 21:25:36 +0530

    Add files via upload

---
+++ Strings/LongestCommonPrefix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+
+        String first = strs[0];
+        String ans = "";
+
+        for (int i = 0; i < first.length(); i++) {
+            String prefix = first.substring(0, i + 1);
+
+            for (int j = 1; j < strs.length; j++) {
+                if (!strs[j].startsWith(prefix)) {
+                    return ans;
+                }
+            }
+            ans = prefix;
+        }
+        return ans;
+    }
+}
