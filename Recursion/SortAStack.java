5d6379e0376a60bb8d8bbbcff75fcf4ff32d67aa
Manish Mahara <manish23mahara@gmail.com>
2026-02-25 23:07:35 +0530

    Add files via upload

---
+++ Recursion/SortAStack.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void sortStack(Stack<Integer> st) {
+        // base case 
+        if(st.isEmpty()) return;
+        
+        // remove top element 
+        int top = st.pop();
+        
+        // sort rest of the stack
+        sortStack(st);
+        
+        // insert top at correct position
+        insert(st,top);
+    }
+    
+    public void insert(Stack<Integer> st, int value){
+        
+        // base case 
+        if(st.isEmpty() || st.peek()<=value){
+            st.push(value);
+            return;
+        }
+        
+        // Remove top and recursion
+        int top = st.pop();
+        insert(st, value);
+        
+        // push back removed element 
+        st.push(top);
+      }
+    }
